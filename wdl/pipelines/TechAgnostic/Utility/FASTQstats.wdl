bshifaw/long-read-pipelines-test

New trivial workflows to   * filter reads by length in a BAM   * collect stats from (length-filtered) FASTQ

wdl/pipelines/TechAgnostic/Utility/FASTQstats.wdl
@@ -1,0 +1,32 @@
+version 1.0
+
+import "../../../tasks/Utility/FastqUtils.wdl" as FQU
+
+workflow FASTQstats {
+    meta {
+        desription:
+        "Collect some basic stats from a FASTQ file."
+    }
+    parameter_meta {
+        fastq: "file to collect stats on"
+        seq_type: "argument to the --seq-type paramter"
+        exclude_len_threshold: "Sequeces shorter than this will be dropped from analysis."
+    }
+
+    input {
+        File fastq
+        String seq_type = "dna"
+        Int? exclude_len_threshold
+    }
+
+    if (defined(exclude_len_threshold)) {
+        call FQU.FilterByLen { input: fastq = fastq, exclude_len_threshold = select_first([exclude_len_threshold])}
+    }
+
+    call FQU.Stats { input: fastq = select_first([FilterByLen.res, fastq]), seq_type = seq_type }
+
+    output {
+        Map[String, Float] stats = Stats.res
+    }
+}
+

nikhil777shingte/long-read-pipelines

wdl/pipelines/TechAgnostic/Utility/FASTQstats.wdl
@@ -1,0 +1,32 @@
+version 1.0
+
+import "../../../tasks/Utility/FastqUtils.wdl" as FQU
+
+workflow FASTQstats {
+    meta {
+        desription:
+        "Collect some basic stats from a FASTQ file."
+    }
+    parameter_meta {
+        fastq: "file to collect stats on"
+        seq_type: "argument to the --seq-type paramter"
+        exclude_len_threshold: "Sequeces shorter than this will be dropped from analysis."
+    }
+
+    input {
+        File fastq
+        String seq_type = "dna"
+        Int? exclude_len_threshold
+    }
+
+    if (defined(exclude_len_threshold)) {
+        call FQU.FilterByLength   { input: fq = fastq, threshold = select_first([exclude_len_threshold])}
+        # call FQU.FilterByLenSeqTk { input: fastq = fastq, exclude_len_threshold = select_first([exclude_len_threshold])}
+    }
+
+    call FQU.Stats { input: fastq = select_first([FilterByLength.res, fastq]), seq_type = seq_type }
+
+    output {
+        Map[String, Float] stats = Stats.res
+    }
+}